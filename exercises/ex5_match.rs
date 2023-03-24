6496d4782650e49f92fa4608616814d84ac3e73b
Martin Brönnimann <martin.bronnimann.pj@gmail.com>
2023-03-24 15:15:06 +0100

    Create ex5_match.rs

---
+++ exercises/ex5_match.rs
@@ -0,0 +1,28 @@
+// Exercise: Write a code which will take: Two variables named a and b, a character type variable called operator which will take operators (+,-,/,*,%) will be passed as input to our match statement. Use match statements to compute: addition of a and b, subtraction of b from a, multiplication of a and b, division of a by b, modulus of a by b
+fn test(a: i32, operator: char, b: i32) {
+   match operator {
+      '+' => { println!("{}", a + b) },
+      '-' => { println!("{}", a - b) },
+      '*' => { println!("{}", a * b) },
+      '/' => { if b == 0 { println!("Division by 0 is undefined") } else { println!("{}", a / b) } },
+      '%' => { println!("{}", a % b) },
+      _ => { println!("Invalid operator") }
+   }
+}
+
+fn main(){
+    print!("3 + 2: ");
+    test(3,'+',2);
+    print!("3 - 2: ");
+    test(3,'-',2);
+    print!("3 * 2: ");
+    test(3,'*',2);
+    print!("3 / 2: ");
+    test(3,'/',2);
+    print!("3 % 2: ");
+    test(3,'%',2);
+    print!("3 ( 2: ");
+    test(3,'(',2);
+    print!("3 ( 0: ");
+    test(3, '/', 0)
+}
